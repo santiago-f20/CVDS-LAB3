93739361320f4e970faa278e99498bcc219687ee
unknown <santiago.fetecua@labinfo.is.escuelaing.edu.co>
2022-02-05 08:41:59 -0500

    Proyecto compilado y ejecutado. Esqueleto de la Aplicacion creado. Primeras pruebas ejecutadas

---
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.cvds.tdd.registry;
+
+/**
+ *
+ * @author santiago.fetecua
+ */
+public class Registry {
+    public RegisterResult registerVoter(Person p) {
+
+        // TODO Validate person and return real result.
+        return RegisterResult.VALID;
+    }
+}
---
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.cvds.tdd.registry;
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ *
+ * @author santiago.fetecua
+ */
+public class RegistryTest {
+    private Registry registry = new Registry();
+
+    @Test
+    public void validateRegistryResult() {
+
+        Person person = new Person();
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+
+    // TODO Complete with more test cases
+}
